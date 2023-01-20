3dadc0946d614cdf0abf9093e9a35c6efa6e0cba
Christopher Paul Haar <christopherpaul.haar@dkb.de>
2023-01-20 11:40:30 +0100

    feat(initial): initital commit
    Signed-off-by: Christopher Paul Haar <christopherpaul.haar@dkb.de>

---
+++ config/opensearchrole/config.go
@@ -0,0 +1,9 @@
+package repository
+
+import "github.com/upbound/upjet/pkg/config"
+
+func Configure(p *config.Provider) {
+	p.AddResourceConfigurator("elasticsearch_opensearch_role", func(r *config.Resource) {
+		r.ShortGroup = "elasticsearch"
+	})
+}
---
+++ config/provider.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2021 Upbound Inc.
+*/
+
+package config
+
+import (
+	// Note(turkenh): we are importing this to embed provider schema document
+	_ "embed"
+
+	ujconfig "github.com/upbound/upjet/pkg/config"
+
+	opensearchrole "github.com/haarchri/provider-opensearch/config/opensearchrole"
+)
+
+const (
+	resourcePrefix = "opensearch"
+	modulePath     = "github.com/haarchri/provider-opensearch"
+)
+
+//go:embed schema.json
+var providerSchema string
+
+//go:embed provider-metadata.yaml
+var providerMetadata string
+
+// GetProvider returns provider configuration
+func GetProvider() *ujconfig.Provider {
+	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
+		ujconfig.WithIncludeList(ExternalNameConfigured()),
+		ujconfig.WithShortName("opensearch"),
+		ujconfig.WithRootGroup("opensearch.crossplane.io"),
+		ujconfig.WithDefaultResourceOptions(
+			ExternalNameConfigurations(),
+		))
+
+	for _, configure := range []func(provider *ujconfig.Provider){
+		// add custom config functions
+		opensearchrole.Configure,
+	} {
+		configure(pc)
+	}
+
+	pc.ConfigureResources()
+	return pc
+}
